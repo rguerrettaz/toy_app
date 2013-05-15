d74223e32a15d0980f2384143fd70c23184bbde3
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-05-15 08:54:28 -0700

    Add sidebar to index page

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,10 @@ $(function(){
   defaultSectionHeight = 600
 
   // sets height for section on document.ready()
+  // sets height for section and sidebar on document.ready()
   setSectionHeight()
+  setSidebarHeight()
+  setSidebarListItemMargins()
 
   // listens for window resize
   $win.resize(resizeSection)
@@ -18,9 +21,13 @@ $(function(){
 
 // sets section height if window height greater than default
 // height
+// sets section and sidebar height if window height greater than
+// default height
 function resizeSection(){
   if($win.height() >= defaultSectionHeight) {
     setSectionHeight()
+    setSidebarHeight()
+    setSidebarListItemMargins()
   }
 }
 
@@ -29,3 +36,11 @@ function setSectionHeight(){
   $('section').css("height", $win.height())
 }
 
+// sets sidebar height based on window.height()
+function setSidebarHeight(){
+  $('.sidebar').css("height", $win.height())
+}
+
+function setSidebarListItemMargins(){
+  $('.navlist li').css("margin-bottom", $win.height() * .174)
+}
