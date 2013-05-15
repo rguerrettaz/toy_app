10b00ea3dba022daef4ac323e0c1d1fa483c67ae
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-05-15 10:36:13 -0700

    Add li color change when scroll enters into connect section

--- public/js/main.js
+++ public/js/main.js
@@ -3,31 +3,32 @@ $(function(){
   $win = $(window)
   defaultSectionHeight = 600
 
-  // sets height for section on document.ready()
+  // REFACTOR ME
   // sets height for section and sidebar on document.ready()
   setSectionHeight()
   setSidebarHeight()
   setSidebarListItemMargins()
+  updateBottomPadding()
 
   // listens for window resize
-  $win.resize(resizeSection)
+  $win.resize(updateSidebarsAndSections)
 
   // controls compass arrow. NEEDS a refactor
   $(document).scroll(function(){
-    $('.compass').css("top", $win.height() * ($win.scrollTop() / $(this).height()) + 30)
+    $('.compass').css("top", $win.height() * ($win.scrollTop() / $(this).height())+ 35)
+    updateConnectSectionTopPosition()
+    updateFontColor()
   })
 });
 
-
-// sets section height if window height greater than default
-// height
 // sets section and sidebar height if window height greater than
 // default height
-function resizeSection(){
+function updateSidebarsAndSections(){
   if($win.height() >= defaultSectionHeight) {
     setSectionHeight()
     setSidebarHeight()
     setSidebarListItemMargins()
+    updateBottomPadding()
   }
 }
 
@@ -42,5 +43,33 @@ function setSidebarHeight(){
 }
 
 function setSidebarListItemMargins(){
-  $('.navlist li').css("margin-bottom", $win.height() * .174)
+  margin = $win.height() * .174
+  $('.navlist li').css("margin-bottom", margin)
 }
+
+function updateConnectSectionTopPosition(){
+  $connectSectionPosition = $('#connect').offset().top - $(window).scrollTop()
+}
+
+function updateBottomPadding(){
+  bottomPadding = $win.height() - $('li:nth-child(5)').offset().top
+}
+
+// REFACTOR ME
+// Updates font color based on position of connect section
+function updateFontColor(){
+  var liList = [5,4,3,2,1]
+
+  for (var i = 0, len = liList.length; i < len; i++) {
+    var $liItem = $('li:nth-child(' + liList[i] + ')')
+    var position = $win.height() - bottomPadding - (margin + 17)   * i
+
+    if (position >= $connectSectionPosition) {
+      $liItem.css("color", "white")
+    }
+    else {
+      $liItem.css("color", "black")
+    }
+  }
+}
+
